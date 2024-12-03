1d32c019566fdd292e24dc89aca683eec26fc6d6
DESKTOP-NM0RBMK\milag <edurinvaz@gmail.com>
2024-12-03 14:30:29 +0100

    he añadido movimiento y velocidad acumulable a los enemigos, ademas de varios prefab para cambiar al enemigo el dia de mañana

---
+++ Bloomie Escape/Assets/scripts/enemigo.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class enemigo : MonoBehaviour
+{
+    [SerializeField] Camera camara;
+    [SerializeField] Vector2 posicionInicial;
+    [SerializeField] Vector2 posicionMinima;
+    [SerializeField] float velocidad;
+
+
+
+    void Start()
+    {
+        camara = Camera.main;
+        posicionMinima = camara.ViewportToWorldPoint(new Vector2(0, 0));
+        posicionInicial= transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(velocidad * Time.deltaTime * Vector2.left);
+
+        if (transform.position.x < posicionMinima.x-1)
+        {
+            transform.position = posicionInicial;
+            if (velocidad <= 25)
+            {
+                velocidad = velocidad + 1;
+            }
+            else 
+            {
+             //do nothing 
+            }
+        }
+    }
+
+}
