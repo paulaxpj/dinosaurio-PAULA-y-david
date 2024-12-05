9d1ec6c0be97ef2acdac2070668ce2dc6aad2576
DESKTOP-NM0RBMK\milag <edurinvaz@gmail.com>
2024-12-05 13:43:22 +0100

    casi toda mi parte hecha me falta solo un

---
+++ Bloomie Escape/Assets/scripts/GameManagerDavid.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManagerDavid : MonoBehaviour
+{
+    [SerializeField] public float tiempo;
+    [SerializeField] TMP_Text textoTiempo;
+
+    [SerializeField] GameObject pantallaGameOver;
+    [SerializeField] GameObject botonReintentar;
+
+    [SerializeField] GameObject Bloomie;
+    [SerializeField] GameObject enemigo;
+
+    [SerializeField] bool activarCronometro;
+
+    [SerializeField] int puntuacionActual;
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pantallaGameOver.SetActive(false);
+        botonReintentar.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (activarCronometro == true)
+        {
+            tiempo += Time.deltaTime;
+            int minutos = (int)tiempo / 60;
+            int segundos = (int)tiempo % 60;
+
+            Debug.Log($"{minutos:D2}:{segundos:D2}");
+
+            textoTiempo.text = $"{minutos:D2}:{segundos:D2}";
+        }
+    }
+
+    public void perder() 
+    { 
+        Bloomie.SetActive(false);
+        enemigo.SetActive(false);
+        pantallaGameOver.SetActive(true);
+        botonReintentar.SetActive(true);
+        activarCronometro = false;
+
+    }
+
+    public void reiniciarJuego() 
+    {
+        puntuacionActual = 0;
+        Bloomie.SetActive(true);
+        enemigo.SetActive(true);
+        pantallaGameOver.SetActive(false);
+        botonReintentar.SetActive(false);
+        tiempo = 0;
+        activarCronometro = true;
+        iniciarEnemigo();
+    }
+}
